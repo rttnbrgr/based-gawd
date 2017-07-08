b9502df6c2118e1efe752b04a6bc1eedf88d8264
Tayo <nroth10@gmail.com>
2017-07-08 11:27:23 -0700

    Add tutorial demo stuff

--- src/App.js
+++ src/App.js
@@ -3,19 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+   render() {
+      return (
+         <div className='app'>
+            <header>
+               <div className='wrapper'>
+                  <h1>Fun Food Friends</h1>
+               </div>
+            </header>
+
+            <div className='container'>
+               <section className='add-item'>
+                  <form>
+                     <input type="text" name="username" placeholder="What's your name?" />
+                     <input type="text" name="currentItem" placeholder="What are you bringing?" />
+                     <button>Add Item</button>
+                  </form>
+               </section>
+               <section className='display-item'>
+                  <div className='wrapper'>
+                     <ul></ul>
+                  </div>
+               </section>
+            </div>
+         </div>
+      );
+   }
 }
 
 export default App;
